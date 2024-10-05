6294b674d4230ecbe7cc5265cf4e564381fe34c8
Ritik-Sharma-Git <99575617+Ritik-Sharma-Git@users.noreply.github.com>
2024-10-06 00:55:03 +0530

    Time: 4 ms (81.5%), Space: 8.9 MB (50.93%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n=s1.size(),m=s2.size();
+        if(n>m)return false;
+
+        vector<int> s1_count(26,0),s2_count(26,0);
+        for(int i=0;i<n;i++){
+            s1_count[s1[i]-'a']++; 
+            s2_count[s2[i]-'a']++;
+        }
+        if(s1_count==s2_count)return true;
+        
+        for(int i=n;i<m;i++){
+            ++s2_count[s2[i]-'a'];
+            --s2_count[s2[i-n]-'a'];
+            if(s1_count==s2_count){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
